be30b30f463ac50687af6c1dd5252a33e71777b5
Maksym Dzhafarov <maksim8797@gmail.com>
2022-01-22 01:17:28 +0200

    Update googleAuth.js

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -7,68 +7,66 @@ const { User } = require("../../models");
 const { SECRET_KEY } = process.env;
 
 const googleAuth = async (req, res) => {
-    const stringifiedParams = queryString.stringify({
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
-        scope: [
-            "https://www.googleapis.com/auth/userinfo.email",
-            "https://www.googleapis.com/auth/userinfo.profile",
-        ].join(" "),
-        response_type: "code",
-        access_type: "offline",
-        prompt: "consent",
-    });
-    return res.redirect(
-        `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
-    );
+  const stringifiedParams = queryString.stringify({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" "),
+    response_type: "code",
+    access_type: "offline",
+    prompt: "consent",
+  });
+  return res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
+  );
 };
 const googleRedirect = async (req, res) => {
-    const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-    const urlObj = new URL(fullUrl);
-    const urlParams = queryString.parse(urlObj.search);
-    const code = urlParams.code;
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+  const urlObj = new URL(fullUrl);
+  const urlParams = queryString.parse(urlObj.search);
+  const code = urlParams.code;
 
-    console.log(code);
+  console.log(code);
 
-    const tokenData = await axios({
-        url: "https://oauth2.googleapis.com/token",
-        method: "post",
-        data: {
-            client_id: process.env.GOOGLE_CLIENT_ID,
-            client_secret: process.env.GOOGLE_CLIENT_SECRET,
-            redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
-            grant_type: "authorization_code",
-            code,
-        },
-    });
-    const userData = await axios({
-        url: "https://www.googleapis.com/oauth2/v2/userinfo",
-        method: "get",
-        headers: {
-            Authorization: `Bearer ${tokenData.data.access_token}`,
-        },
-    });
-    const { email } = userData.data;
-    let user = await User.findOne({ email });
-    const avatarURL = gravatar.url(email);
-    
+  const tokenData = await axios({
+    url: "https://oauth2.googleapis.com/token",
+    method: "post",
+    data: {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
+      grant_type: "authorization_code",
+      code,
+    },
+  });
+  const userData = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${tokenData.data.access_token}`,
+    },
+  });
+  const { email } = userData.data;
+  let user = await User.findOne({ email });
+  const avatarURL = gravatar.url(email);
+
   if (!user) {
     user = await User.create({
-    email,
-    avatarURL,
-  });
-    };
+      email,
+      avatarURL,
+    });
+  }
 
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
   user = await User.findByIdAndUpdate(user._id, { token });
 
-    return res.redirect(
-        `${process.env.FRONTEND_URL}?token=${token}`
-    )
+  return res.redirect(`${process.env.FRONTEND_URL}/expenses?token=${token}`);
 };
 
 module.exports = {
-    googleAuth,
-    googleRedirect,
+  googleAuth,
+  googleRedirect,
 };
